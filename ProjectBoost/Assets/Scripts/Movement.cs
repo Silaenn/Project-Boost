de401be87909aa9ff99bd38887093090621de30a
Deo silaen <deokeldisilaen@gmail.com>
2025-01-29 09:02:45 +0700

    addAudioRocket

--- ProjectBoost/Assets/Scripts/Movement.cs
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -5,9 +5,11 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+    AudioSource audioSource;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -21,6 +23,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
